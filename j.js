b7de87459b5934f71fa89e044ea9d8a387096b3e
youness <73246981+saghouchni@users.noreply.github.com>
2021-07-02 16:31:59 +0200

    Add files via upload
    i just learn,
    am lon the right path

---
+++ j.js
@@ -0,0 +1,332 @@
+
+"use stict";
+ 
+$(document).ready(function () {
+ 
+ $('title').html('hlima');
+ $('input:first').css('width', '400px');
+ $('input:first').css('height', '40px');
+ $('input:first').css('fontSize', '30px');
+ $('input:first').css('textAlign', 'center');
+ $('input:first').css('fontStyle', 'italic');
+ $('input:submit').css('textAlign','center');
+});
+ 
+ 
+ 
+$('#xlxl').css({
+ 'width': '100%',
+ 'height': '300px',
+ 'textAlign': 'center',
+ 'border': 'none',
+ 'paddingTop':'30px'
+  
+});
+ 
+  
+ 
+$(window).ready(function () {
+
+ $('#t1').css('display', 'block');
+ $('#int').css('width', '800px');
+ $('#int').css('border', 'solid 5px #ccc');
+ $('#int').focus(function () {
+  
+  $('#int').attr('placeholder','');
+
+ });
+ $('#int').mouseleave(function () {
+  $('#int').attr('placeholder', 'choose a suitable image for your project');
+
+ });
+ $('#int').click(function () {
+  
+  $('#int').attr('placeholder', '');
+ })
+//  $('#sub').css({
+// 'position':'absolute',
+//   'fontSize': '28px',
+//   'textAlign':' center',
+//   'margin': '0%',
+//   'right': '215px',
+//   'top': '263px',
+//  'padding':'5px'
+
+
+
+
+//  });
+ $('#hyh').css({'position':'absolute'});
+ $('#hyh').css({ 'top': '250px' });
+ $('#hyh').css({ 'textAlign': 'left' });
+ $('#hyh').css({ 'left': '245px' });
+ $('#hyh').css({ 'width': '800px' });
+ $('#hyh').css({
+  'fontStyle': 'oblique',
+  'padding': '3px',
+  'border':'solid 8px #ccc'
+ });
+ $('#v').css({
+  'position': 'absolute',
+  'top': '250px',
+  'right': '220px',
+  'fontSize': '30px',
+  'padding': '5px',
+  'border':'solid 7px #ccc'
+ });
+ $('#v').click(function () {
+  $(this).css({ 'border': 'solid 7px #ccc' });
+ });
+ 
+ $('#hyh').focus(function () {
+  
+  $('#hyh').attr('placeholder','');
+
+ });
+ $('#hyh').mouseleave(function () {
+  $('#hyh').attr('placeholder', 'search');
+
+ });
+ $('#hyh').click(function () {
+  
+  $('#hyh').attr('placeholder','');
+ });
+  
+
+ 
+
+});
+$(window).ready(function () {
+  
+ $('#yyy').css({
+'position':'absolute',
+  'width': '800px',
+  'fontStyle': 'pubique',
+  'margin': '0px',
+  'border': 'solid 5px #ccc',
+  'top':'298px'
+ });
+ $('#xwxw').css({
+'position':'absolute',
+  'top': '299px',
+  'right': '463px',
+  'fontSize': '28px',
+  'padding': '5px',
+  'border':'solid 4px #ccc'
+
+
+ });
+ $('#xwxw').click(function () {
+  
+  $('#xwxw').css({ 'border': 'solid 4px #ccc' });
+ });
+ 
+ 
+});
+$(window).ready(function () {
+ $('#yyy').focus(function () {
+  $('#yyy').attr('placeholder','');
+ });
+   
+});
+$(window).ready(function () {
+  $('#company').css({
+'position':'absolute',
+    'width': '800px',
+    'top': '230px',
+    'fontStyle': 'oblique',
+    'textAlign': 'left',
+    'left': '200px',
+    'border': 'solid 3px #ccc',
+    'margin': '0px',
+    'padding':'5px'
+
+
+
+  });
+  $('#nbl').css({
+    'top': '229px',
+    'fontSize': '30px',
+    'position': 'absolute',
+    'right': '266px',
+    'padding':'9px'
+  
+  
+  });
+  $('#ziro').css({
+    'width': '800px',
+
+
+
+  });
+  $('#photo965iopn').css({
+    'position':'absolute',
+    'top': '370px',
+    'width': '100%',
+    'height': '40px',
+    'textAlign':'center',
+  });
+  $('#int').focus(function () {
+    
+    $(this).attr('placeholder', '');
+
+  });
+  $('#ziro').focus(function(){
+    $('#ziro').attr('placeholder', '');
+    $('#ziro').mouseleave(function () {
+      
+      $(this).attr('placeholder', 'shoose your domain');
+
+    });
+  });
+  $('#yyy').mouseleave(function () {
+    
+    $(this).attr('placeholder', 'Choose a design that suits you');
+    
+
+  });
+
+$('#yyy').mouseleave(function () {
+      
+  $(this).attr('placeholder', 'Choose a design that suits you');
+
+});
+  $('#company').focus(function () {
+    
+    $(this).attr('placeholder', '');
+
+
+  });
+  $('#company').mouseleave(function () {
+    
+    $(this).attr('placeholder', 'Enter a company name');
+
+  });
+  $('.king').css({
+    'position':'absolute',
+    'width': '100%',
+    'height': '300px',
+    'margin': '0px',
+    'padding':'0px',
+
+
+
+
+  });
+  $('#logo').css({
+'position':'absolute',
+    'width': '800px',
+    'top':'300px',
+
+
+
+  });
+  $('#logo').focus(function(){
+    $(this).attr('placeholder', '');
+
+  });
+
+  $('#logo').mouseleave(function(){
+    $(this).attr('placeholder', 'choose your logo');
+    
+  });
+  
+});
+$(window).ready(function () {
+  
+  $('#first').css({
+    "margin-top": '-=30px',
+    'paddingLeft': '-=10px',
+    'marginLeft':'-=20px'
+  });
+  $('.hag99').css({
+    "height":"+=200px"
+  });
+  $('#first').css({
+
+    "height": "+=215px",
+    "width":'1280px'
+  });
+  $('#hag1').siblings().focus(function () {
+    
+    $(this).attr('placeholder', '');
+
+  });
+  $('#hag1').focus(function () {
+    
+    $(this).attr('placeholder', '');
+
+  });
+  $('.xxx').css({
+    "width": "1280",
+    "margin":'0px'
+
+  });
+  $('#sub').css({
+    "paddingBottom": '5px',
+    "top": '+=10px',
+    'border':'solid 3px #ccc'
+
+
+  });
+  // $('"sub').click(function () {
+  //   $(this).css({
+  //     "border":"none"
+  //   });
+  //})
+  $('.hiphop').css({
+    
+    'position':'absolute',
+    'margin': '0px',
+    'marginLeft': '0px',
+    'marginTop': '0px',
+    'marginRight': '0px',
+    "width":'1280px'
+  })
+  $('#test').css({
+
+    'position':'relative',
+    'margin': '0px',
+    'marginLeft': '0px',
+    'marginTop': '0px',
+    'marginRight': '0px',
+    "width":'1280px'
+
+
+
+  });
+  $('#what:ul li a').css({
+    'position':'absolute',
+   "top":"+=200px"
+  });
+  $('.love').css({
+
+    "width": '1280px',
+    "backgroundColor":"white"
+
+  });
+  $('.hg').css({
+
+    "width": '1280px',
+    "backgroundColor":"white"
+
+  });
+  $('.bolacha3la:*').css({
+    "backgroundColor":'white'
+  });
+  
+});
+$(window).ready(function () {
+  $('#what ').css({
+
+    "marginTop": "+=18px",
+     "height":"+=15px"
+
+  });
+  $('#youness-saghouchni').css({
+    "top": "-=16px", 'height': '+=20px'
+  });
+  $('#iloveAmerica').css({'fontSize':"30px"})
+
+   
+ })
